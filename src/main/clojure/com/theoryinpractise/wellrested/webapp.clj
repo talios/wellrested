79d4311ce8b9599313c5c2422ec67d158ae02493
Mark Derricutt <mark@talios.com>
2010-03-26 10:03:27 +1300

    Added OPTIONS/PATCH support

--- src/main/clojure/com/theoryinpractise/wellrested/webapp.clj
+++ src/main/clojure/com/theoryinpractise/wellrested/webapp.clj
@@ -15,16 +15,39 @@
   (:use compojure.map-utils)
   (:use compojure.validation))
 
+(defmacro OPTIONS "Generate an OPTIONS route."
+  [path & body]
+  (compile-route :options path body))
+
+(defmacro PATCH "Generate a PATCH route."
+  [path & body]
+  (compile-route :patch path body))
+
 (defroutes wellrested-app
 
-    (ANY "*"
+    (GET "/"
 		{:status 200
 		 :headers {"Content-Type" "application/wellrested+json"}
 		 :body (json-str {:name "wellrested" 
-						  :links [{:rel "quicktask" :url "./quicktask1" :type "application/wellrested-task+json" :alt "Do something task"}]}
-		
-		
-		)})
+						  :links [{:rel "quicktask" :url "./quicktask1" :type "application/wellrested-task+json" :alt "Do something task"}]})})
+						
+						
+	(GET "/quicktask1"
+		{:status 200
+		 :headers {"Content-Type" "application/quicktask+json"
+		           "Accept-Patch" "application/quicktask-request+json"}
+		 :body (json-str {:name "quicktask1"})})
+						
+    (OPTIONS "/quicktask1"
+		{:status 200
+		 :headers {"Accept-Patch" "application/quicktask-request+json"}})
+
+    (PATCH "/quicktask1"
+        (if (= "application/quicktask-request+json" (:content-type request))
+			{:status 200 :body (json-str {:status "Patched"})}
+            {:status 400 :body (json-str {:status "Unknown patch type"})}))
+	
+	 (ANY "*" {:status 404})
    )
 
 (defservice wellrested-app)
